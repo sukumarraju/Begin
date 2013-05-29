f79f36d978966bbbf97cf6351433ede95d6d730e
Raju Golla <sukumarraju6@gmail.com>
2013-05-29 01:36:27 +0100

    first commit

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,69 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+        private const string CacheKey = "ContactStore";
+
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if (ctx.Cache["ContactStore"] == null)
+                {
+                    var contacts = new Contact[]{
+                    
+                        new Contact{ID=1, Name="Glenn Block"},
+                        new Contact{ID=2, Name="Dan Roth"}
+                    };
+
+                    ctx.Cache[CacheKey] = contacts;
+                }
+            }
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[CacheKey];
+            }
+            return new Contact[]
+            {
+                new Contact{ID=0, Name="Place holder"}
+            };
+        }
+
+        public bool SaveContact(Contact contact)
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                try
+                {
+                    var currentdata = ((Contact[])ctx.Cache[CacheKey]).ToList();
+                    currentdata.Add(contact);
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    return false;
+                }
+            }
+
+            return false;
+        }
+    }
+}
